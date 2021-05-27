364dddd1cd6857dbcf5fb094f4ca4b43d23ea3a4
Jim Anders <jimanders223@gmail.com>
2021-05-27 09:34:59 -0400

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,121 @@
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+
+cmd 'packadd paq-nvim'               -- load the package manager
+local paq = require('paq-nvim').paq  -- a convenient alias
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+-- paq {'shougo/deoplete-lsp'}
+-- paq {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins']}
+paq {'nvim-treesitter/nvim-treesitter'}
+paq {'neovim/nvim-lspconfig'}
+paq {'kabouzeid/nvim-lspinstall'}
+paq {'junegunn/fzf', run = fn['fzf#install']}
+paq {'junegunn/fzf.vim'}
+paq {'ojroques/nvim-lspfuzzy'}
+paq {'dracula/vim'}
+paq {'hrsh7th/nvim-compe'}
+-- g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
+
+vim.g.mapleader = ','
+
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+local indent = 2
+cmd 'colorscheme dracula'                              -- Put your favorite colorscheme here
+opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
+opt('b', 'shiftwidth', indent)                        -- Size of an indent
+opt('b', 'smartindent', true)                         -- Insert indents automatically
+opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
+opt('o', 'completeopt', 'menuone,noselect')  -- Completion options (for deoplete)
+opt('o', 'hidden', true)                              -- Enable modified buffers in background
+opt('o', 'ignorecase', true)                          -- Ignore case
+opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
+opt('o', 'scrolloff', 4 )                             -- Lines of context
+opt('o', 'shiftround', true)                          -- Round indent
+opt('o', 'sidescrolloff', 8 )                         -- Columns of context
+opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
+opt('o', 'splitbelow', true)                          -- Put new windows below current
+opt('o', 'splitright', true)                          -- Put new windows right of current
+opt('o', 'termguicolors', true)                       -- True color support
+opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
+opt('w', 'list', true)                                -- Show some invisible characters (tabs...)
+opt('w', 'number', true)                              -- Print line number
+opt('w', 'relativenumber', true)                      -- Relative line numbers
+opt('w', 'wrap', false)
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('i', 'jk', '<esc>')
+map('', '<leader>c', '"+y')       -- Copy to clipboard in normal, visual, select and operator modes
+map('i', '<C-u>', '<C-g>u<C-u>')  -- Make <C-u> undo-friendly
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+
+-- <Tab> to navigate the completion menu
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+
+map('n', '<C-l>', '<cmd>noh<CR>')    -- Clear highlights
+map('n', '<leader>o', 'm`o<Esc>``')  -- Insert a newline in normal mode
+
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+
+local lsp = require 'lspconfig'
+local lspfuzzy = require 'lspfuzzy'
+local lspinstall = require 'lspinstall'
+lspinstall.setup() -- important
+
+local servers = lspinstall.installed_servers()
+for _, server in pairs(servers) do
+  lsp[server].setup{}
+end
+
+-- For ccls we use the default settings
+lspfuzzy.setup {}  -- Make the LSP client use FZF instead of the quickfix list
+
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+  };
+}
+
+map('n', '<leader>p', '<cmd>:GFiles<CR>')
