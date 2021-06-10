fa7a965ff525e5d1c15d526a7fd09c63180e4d80
Jim Anders <jimanders223@gmail.com>
2021-06-10 08:00:31 -0400

    Updates

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -138,6 +138,10 @@
         bind | split-window -h
         bind - split-window -v
 
+        # reload nix provided config
+        unbind R
+        bind R source-file /etc/tmux.conf
+
         # colors
         set -g default-terminal "screen-256color"
 
@@ -160,12 +164,6 @@
         set -g @plugin 'christoomey/vim-tmux-navigator'
         set -g @plugin 'tmux-plugins/tmux-pain-control'
         set -g @plugin 'tmux-plugins/tmux-urlview'
-
-
-        # Other examples:
-        # set -g @plugin 'github_username/plugin_name'
-        # set -g @plugin 'git@github.com:user/plugin'
-        # set -g @plugin 'git@bitbucket.com:user/plugin'
 
         # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
         run '~/.tmux/plugins/tpm/tpm'
--- .zshrc
+++ .zshrc
@@ -1,3 +1,4 @@
 source "$HOME"/.nix-profile/etc/profile.d/nix.sh
+source "$HOME"/.functions
 eval "$(starship init zsh)"
 eval "$(direnv hook zsh)"
