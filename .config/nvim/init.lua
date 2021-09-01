aed80e1c9ce3b090b54cd74f93b71302697fc604
Jim Anders <jimanders223@gmail.com>
2021-09-01 09:11:35 -0400

    Set some other opts that I have been missing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,6 +10,17 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.incsearch = true
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 1000
+vim.opt.hidden = true
+vim.opt.scrolloff = 10
 
 vim.g.mapleader = ","
 
