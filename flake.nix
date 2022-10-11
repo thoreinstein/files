d9dec86beb3c25e2ab65e4f97680e1c4b4b58021
janders223 <719823+janders223@users.noreply.github.com>
2022-10-10 20:16:40 -0400

    First attempt to flake this

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A flake for building Hello World";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpks-unstable;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-darwin =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-darwin"; };
+      stdenv.mkDerivation {
+        name = "dotfiles";
+        src = self;
+        installPhase = ''
+        ${pkgs.stow}/bin/stow bin
+        ${pkgs.stow}/bin/stow config
+        ${pkgs.stow}/bin/stow direnv
+        ${pkgs.stow}/bin/stow git
+        ${pkgs.stow}/bin/stow gnupg
+        ${pkgs.stow}/bin/stow nvim
+        ${pkgs.stow}/bin/stow tmux
+        ${pkgs.stow}/bin/stow zsh
+        '';
+      };
+
+  };
+}
